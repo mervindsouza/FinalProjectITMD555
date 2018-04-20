0c990ccc1189e11f043d9b7c9ed23acec25000fe
mdsouza5 <mdsouza5@hawk.iit.edu>
2018-04-20 08:20:00 -0500

    🏗 BaseActivity Added
    BaseActivity Added #15

---
+++ app/src/main/java/mdsouza5/finalprojectitmd555/BaseActivity.java
@@ -0,0 +1,16 @@
+package mdsouza5.finalprojectitmd555;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class BaseActivity extends AppCompatActivity {
+    //This is optional
+    // private ProgressBar fpProgressBar;
+
+    public String GetFirebaseUserId() {
+        return FirebaseAuth.getInstance().getCurrentUser().getUid();
+    }
+
+}
