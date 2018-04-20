258bae6b85b362b0c32bed6e2ac744239f7aae78
mdsouza5 <mdsouza5@hawk.iit.edu>
2018-04-20 12:38:03 -0500

    🚧 RecipeHolder Binding To Recipe
    RecipeHolder Binding To Recipe

--- app/src/main/java/mdsouza5/finalprojectitmd555/recipieholder/RecipieViewHolder.java
+++ app/src/main/java/mdsouza5/finalprojectitmd555/recipieholder/RecipieViewHolder.java
@@ -12,12 +12,29 @@ import mdsouza5.finalprojectitmd555.models.Recipes;
 public class RecipieViewHolder extends RecyclerView.ViewHolder {
 
     public TextView recipeTitleTextView;
-    public TextView recipeAuthotTextView;
+    public TextView recipeAuthorTextView;
     public ImageView recipeStarsImageView;
     public TextView recipeStarCountTextView;
     public TextView recipeBodyTextView;
 
     public RecipieViewHolder(View itemView) {
         super(itemView);
+
+        //Insert findViewById
+        //recipeTitleTextView = itemView.findViewById();
+        //recipeAuthorTextView = itemView.findViewById();
+        //recipeStarsImageView = itemView.findViewById();
+        //recipeStarCountTextView = itemView.findViewById();
+        //recipeBodyTextView = itemView.findViewById();
+    }
+
+    //Binding RecipieViewHolder to Recipe Model
+    public void BindingToRecipie(Recipes recipeObj, View.OnClickListener starsClickListener) {
+        recipeTitleTextView.setText(recipeObj.recipeTitle);
+        recipeAuthorTextView.setText(recipeObj.recipeAuthor);
+        recipeStarCountTextView.setText(String.valueOf(recipeObj.starredRecipesCount));
+        recipeBodyTextView.setText(recipeObj.recipeBody);
+
+        recipeStarsImageView.setOnClickListener(starsClickListener);
     }
 }
