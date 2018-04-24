d28aefd2523e9397576896f9344b6c107856f77c
mdsouza5 <mdsouza5@hawk.iit.edu>
2018-04-24 14:19:28 -0500

    📌 Updated Firebase Dependencies
    Updated Firebase Dependencies

--- app/src/main/java/mdsouza5/finalprojectitmd555/recipefragments/MyRecipesFragment.java
+++ app/src/main/java/mdsouza5/finalprojectitmd555/recipefragments/MyRecipesFragment.java
@@ -1,4 +1,13 @@
 package mdsouza5.finalprojectitmd555.recipefragments;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
 public class MyRecipesFragment extends RecipeListFragment {
+    public MyRecipesFragment() {
+    }
+
+//    public Query GetQuery(DatabaseReference fpDatabaseReference){
+//        return fpDatabaseReference.child("user-recipes").child(get());
+//    }
 }
--- app/src/main/java/mdsouza5/finalprojectitmd555/recipefragments/RecipeListFragment.java
+++ app/src/main/java/mdsouza5/finalprojectitmd555/recipefragments/RecipeListFragment.java
@@ -1,6 +1,31 @@
 package mdsouza5.finalprojectitmd555.recipefragments;
 
 import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.Transaction;
+import com.google.firebase.database.R;
+
+import mdsouza5.finalprojectitmd555.models.Recipes;
+import mdsouza5.finalprojectitmd555.recipieholder.RecipieViewHolder;
+import mdsouza5.finalprojectitmd555.RecipeDetailActivity;
 
 public class RecipeListFragment extends Fragment {
 }
