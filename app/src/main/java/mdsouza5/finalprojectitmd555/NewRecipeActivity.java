a62422f54affe40c43846c8ec3acbe3e037470cc
mdsouza5 <mdsouza5@hawk.iit.edu>
2018-04-25 08:18:31 -0500

    ✨ Updated NewRecipeActivity & Activity
    Updated NewRecipeActivity & Activity

--- app/src/main/java/mdsouza5/finalprojectitmd555/NewRecipeActivity.java
+++ app/src/main/java/mdsouza5/finalprojectitmd555/NewRecipeActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class NewRecipeActivity extends BaseActivity {
 
@@ -21,5 +22,13 @@ public class NewRecipeActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_recipe);
+
+        fpDatabaseReference = FirebaseDatabase.getInstance().getReference();
+
+        fpRecipeTitleField = findViewById(R.id.field_recipe_title);
+        fpRecipeBodyField = findViewById(R.id.field_recipe_body);
+        fpSubmitButton = findViewById(R.id.fab_submit_recipe);
+
     }
 }
