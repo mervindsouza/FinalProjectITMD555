b6d2e3556fa2eeb524176e27d2ed7c32c2a413c4
mdsouza5 <mdsouza5@hawk.iit.edu>
2018-04-25 07:56:02 -0500

    ➕ Added Design Dependency
    Added Design Dependency

--- app/src/main/java/mdsouza5/finalprojectitmd555/NewRecipeActivity.java
+++ app/src/main/java/mdsouza5/finalprojectitmd555/NewRecipeActivity.java
@@ -1,4 +1,25 @@
 package mdsouza5.finalprojectitmd555;
 
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.annotation.Nullable;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+
 public class NewRecipeActivity extends BaseActivity {
+
+    private static final String LOGTAG = "NewRecipeActivity";
+    private static final String REQUIRED_VALUE = "Required";
+
+    private DatabaseReference fpDatabaseReference;
+
+    private EditText fpRecipeTitleField;
+    private EditText fpRecipeBodyField;
+    private FloatingActionButton fpSubmitButton;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
